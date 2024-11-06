4088eead6f5c6ba63753d9a7304333e4d99906a9
nguyenvinhloi52@gmail.com <nguyenvinhloi52@gmail.com>
2024-11-06 16:17:50 +0700

    update mysql & sqlalchemy

--- saleapp/app/dao.py
+++ saleapp/app/dao.py
@@ -1,33 +1,11 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
-from sqlalchemy.orm import relationship
-from app import db, app
+from app.models import Category
+
+def load_categories():
+    return Category.query.order_by("id").all()
 
 
-class Category(db.Model):
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(50), nullable=False, unique=True)
-    products = relationship('Product', backref='category', lazy=True)
-
-class Product(db.Model):
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(50), nullable=False)
-    description = Column(String(255), nullable=True)
-    price = Column(Float, default=0)
-    image = Column(String(100), nullable=True)
-    active = Column(Boolean, default=True)
-    category_id = Column(Integer, ForeignKey(Category.id), nullable=False)
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-        #c1 = Category(name='Mobile')
-        #c2 = Category(name='Desktop')
-        #c3 = Category(name='Tablet')
-
-        #db.session.add_all([c1, c2, c3])
-        #db.session.commit()
-
-        products = [{
+def load_products():
+    return [{
         "id": 1,
         "name": "iPhone 7 Plus",
         "description": "Apple, 32GB, RAM: 3GB, iOS13",
@@ -91,9 +69,3 @@ if __name__ == '__main__':
             "category_id": 2
         }
     ]
-
-    for p in products:
-        prod = Product(**p)
-        db.session.add(prod)
-
-    db.session.commit()
